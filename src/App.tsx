bc1522438bb39a43c0784fd0b27dc392589ac9c8
jurco-exe <jurajbosy1@gmail.com>
2023-10-31 23:16:39 +0100

    feat: changing nicknames fuctionality is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Player from './components.tsx/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<section id="game-container">
+			<ol id="players">
+				<Player initialName={'Player1'} symbol={'X'} />
+				<Player initialName={'Player2'} symbol={'O'} />
+			</ol>
+			GAME BOARD
+		</section>
+	);
+};
 
 export default App;
---
+++ src/components.tsx/Player.tsx
@@ -0,0 +1,55 @@
+import { log } from 'console';
+import React, { ReactNode, useState } from 'react';
+
+type Props = {
+	initialName: string;
+	symbol: string;
+};
+
+const Player = ({ initialName, symbol }: Props) => {
+	// !TODO ---- useState isEditing, setIsEditing
+	// add func. thats triggered when the button is clicked
+	// change isEditing to true in that function
+	// show the <span classname='player-name'> only when isEditing == false
+	// show an <input> element when isEditing == true
+	const [isEditing, setIsEditing] = useState<boolean>(false);
+
+	const [playerName, setPlayerName] = useState(initialName);
+
+	const handleClick = () => {
+		setIsEditing((editing) => !editing);
+	};
+
+	const handleChange = (e: any) => {
+		setPlayerName(e.target.value);
+	};
+
+	const BUTTON_EDIT = 'edit';
+	const BUTTON_SAVE = 'save';
+
+	const CONST_PLAYER_NAME = <span className="player-name">{playerName}</span>;
+	const CONST_INPUT = (
+		<input
+			placeholder="select your name"
+			required
+			value={playerName}
+			onChange={handleChange}
+		/>
+	);
+
+	return (
+		<div className="wrapper">
+			<li>
+				<span className="player">
+					{!isEditing ? CONST_PLAYER_NAME : CONST_INPUT}
+					<span className="player-symbol">{symbol}</span>
+				</span>
+				<button onClick={() => handleClick()}>
+					{!isEditing ? BUTTON_EDIT : BUTTON_SAVE}
+				</button>
+			</li>
+		</div>
+	);
+};
+
+export default Player;
